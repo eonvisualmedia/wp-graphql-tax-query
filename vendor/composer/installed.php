f80eb892c4374c07256836f34fe2885ee4592250
Michael Nightingale <mnightingale@eon-media.com>
2022-05-31 11:28:37 +0100

    Include some of vendor directory to support installs from Packagist

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '1cfb2a1dc7c06360fc49578a83bcae6a210179fb',
+        'name' => 'eonvisualmedia/wp-graphql-tax-query',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'eonvisualmedia/wp-graphql-tax-query' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '1cfb2a1dc7c06360fc49578a83bcae6a210179fb',
+            'dev_requirement' => false,
+        ),
+    ),
+);
